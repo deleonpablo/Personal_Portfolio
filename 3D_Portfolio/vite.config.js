0e28bb7f24561fac45358df1e27ed6ee31018342
Pablo De Leon <p.del@uniandes.edu.co>
2025-12-23 22:36:38 -0500

    Crecion de alias de en vite config para adaptar la pagina a React19

--- 3D_Portfolio/vite.config.js
+++ 3D_Portfolio/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Esto le dice a Vite: "Cuando alguien pida react-dom/client, dáselo desde aquí"
+      'react-dom/client': 'react-dom/client',
+      'react-dom': 'react-dom',
+      'react': 'react',
+    },
+  },
 })
