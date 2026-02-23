4db931193fb6718258b873d6b61892e64203a1e5
Pablo De Leon <p.del@uniandes.edu.co>
2026-02-23 00:07:05 -0500

    Primeros hooks

--- 3D_Portfolio/src/pages/Contact.jsx
+++ 3D_Portfolio/src/pages/Contact.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useRef } from 'react'
 import emailjs from '@emailjs/browser';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
 
 const Contact = () => {
   const formRef = useRef(null);
-  const [form, setForm] = useState({name: '', email: '', message: ''})
+  const [form, setForm] = useState({name: '', email : '', message: ''})
   const [isLoading, setIsLoading] = useState(false);
+
+  const {alert, showAlert, hideAlert} = useAlert(); 
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value})
@@ -27,14 +31,19 @@ const Contact = () => {
        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     ).then(() => {
       setIsLoading(false);
-      //TODO: Set show succes message 
-      //TODO: hide  an alert.
+      showAlert({show: true, text: 'Message sent successfully!', type: 'success'})
 
-      setForm({name: '', email: '', message: '' })
+      setTimeout (() => {
+
+        hideAlert();
+        setForm({name: '', email: '', message: '' });
+
+      },[3000])
+
     }).catch((error) => {
       setIsLoading(false);
       console.log(error);
-      //TODO: Show error message.
+      showAlert({show: true, text: 'I didnt receive your message', type: 'danger'})
     })
   };
   const handleFocus =() => {};
@@ -46,6 +55,8 @@ const Contact = () => {
   return (
     <section className="relative flex lg:flex-row flex-col
     max-container ">
+      {alert.show && <Alert {... alert} />}
+      <Alert {... alert} />
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className ="head-text">
           Get in Touch. </h1>
